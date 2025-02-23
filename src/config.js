5b97f1e5a7680d37dcb17996d01d780ed44b5fcb
niteshrawat0401 <niteshrawat0401@gmail.com>
2025-02-23 19:52:01 +0530

    changes in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const apiUrl = import.meta.env.DEV
-  ? import.meta.env.VITE_BASE_URL
-  : import.meta.env.VITE_PROD_API_URL;
+const apiUrl = import.meta.env.VITE_ENV === 'production' 
+               ? import.meta.env.VITE_PROD_API_URL
+               : import.meta.env.VITE_BASE_URL;
 
 export default apiUrl;
