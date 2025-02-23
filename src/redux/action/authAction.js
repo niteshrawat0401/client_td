bbf241a4a3563a2ac2f2dff947ec4c509eca67a0
niteshrawat0401 <niteshrawat0401@gmail.com>
2025-02-24 00:55:14 +0530

    remove comment

--- src/redux/action/authAction.js
+++ src/redux/action/authAction.js
@@ -9,7 +9,6 @@ export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
 export const LOGOUT = "LOGOUT";
 
-// Backend API URL
 const API_URL = apiUrl;
 // console.log(import.meta.env)
 
